f88e57bfe8aa56b7cabec92fae261be93318b6fd
Nate <48927902+Nlichauco@users.noreply.github.com>
2021-01-08 04:19:41 -0500

    Add files via upload

--- AutoPlot.py
+++ AutoPlot.py
@@ -21,6 +21,7 @@ def GetCols(filename,col):
 def makePlot(filenames):
     fig = go.Figure()
     weeks=GetCols(filenames[0],0)
+    fig.update_layout(title=dict(text="",))
     for i in range(0,len(filenames)):
         fname=filenames[i]
         data=GetCols(filenames[i],19)
@@ -28,6 +29,25 @@ def makePlot(filenames):
         fig.add_trace(go.Scatter(x=weeks, y=data, name=traceName))
     fig.update_xaxes(title_text="Weeks",title_font_size=18)
     fig.update_yaxes(title_text="Analytical Tone Scores",title_font_size=18,range=[.48,1])
+
+
+    #fig.update_layout(legend_title_text="Weekly Average Analytical Tone Score per Primary Desk")
+    # fig.update_layout(legend_title=dict(
+    # text="Weekly Average Analytical Tone Score per Primary Desk"
+    # ),legend_title_font_size=20,legend_title_side="")
+    fig.update_layout(title_text="Weekly Average Analytical Tone Score per Primary Desk")
+    fig.update_layout(title_x=.1)
+    fig.update_layout(title_font_size=20)
+    fig.update_layout(legend=dict(
+    orientation="h",
+    yanchor="bottom",
+    y=1.02,
+    xanchor="right",
+    x=1
+    ))
+
+    #legend_title_side="top"
+    #fig.update_layout(legend_orientation="h")
     fig.show()
 
 
@@ -41,10 +61,4 @@ def main():
 
 
 main()
-    #
-    #
-    # trace0 = go.Scatter(x=[1, 2, 3, 4],y=[10, 15, 13, 17])
-    # trace1 = go.Scatter(x=[1, 2, 3, 4], y=[16, 5, 11, 9])
-    # data = [trace0, trace1]
-    #
-    # py.plot(data, filename = 'basic-line', auto_open=True)
+    
