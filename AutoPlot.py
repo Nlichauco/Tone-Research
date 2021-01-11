4c6df1622cf7d54906b0c0796a498cf024594392
Nathaniel Lichauco <nathaniel@nathaniels-mbp.myfiosgateway.com>
2021-01-10 22:58:29 -0500

    update title and sizing

--- AutoPlot.py
+++ AutoPlot.py
@@ -1,7 +1,6 @@
 import plotly.graph_objects as go
 import pandas as pd
 from plotly.validators.scatter.marker import SymbolValidator
-from IPython.display import Image
 symbols = []
 raw_symbols = SymbolValidator().values
 for i in range(0, len(raw_symbols), 12):
@@ -28,14 +27,13 @@ def makePlot(filenames, country, tone):
         traceName = fname[:fname[i].find(".")]
         fig.add_trace(
             go.Scatter(x=weeks, y=data, name=traceName, yaxis="y", mode='lines+markers', marker_symbol=symbols[i]))
-    fig.update_xaxes(title_text="Weeks", title_font_size=18, tickangle=90,showgrid=False,range=[-.25, 42.15])
-    fig.update_yaxes(title_text=tone + " Tone Scores", title_font_size=18, range=[.5, 1],showgrid=False)
-    # fig = make_subplots(specs=[[{"secondary_y": True}]])
-    Covid_Data = GetCols("/Users/nathaniel/Desktop/Tone-Research/res/CovidData/UKcovidAVG.csv", 0)
+    fig.update_xaxes(title_text="Weeks", title_font_size=18, tickangle=90,range=[-.25, 42.15])
+    fig.update_yaxes(title_text=tone + " Tone Scores", title_font_size=18, range=[.5, 1])
+    Covid_Data = GetCols("res/CovidData/UKcovidAVG.csv", 0)
     fig.update_layout(yaxis2=dict(
         title="Weekly Average Covid Cases in the " + country,
         titlefont=dict(
-            color="#d62728"
+            size=17
         ),
         range=[0, 50000],
         anchor="x",
@@ -43,7 +41,7 @@ def makePlot(filenames, country, tone):
         side="right"
     ))
     fig.add_trace(go.Scatter(x=weeks, y=Covid_Data, name='Covid cases', yaxis='y2', fill='tozeroy'))
-    fig.update_layout(title_text="Weekly Average Analytical Tone Score per Primary Desk")
+    fig.update_layout(title_text="<b>(The Guardian) Weekly Average Analytical Tone Score per Primary Desk</b>")
     fig.update_layout(title_x=.1,paper_bgcolor='rgba(0,0,0,0)',
     plot_bgcolor='rgba(0,0,0,0)')
     fig.update_layout(title_font_size=20,width=900,height=520, autosize=True,margin=dict(
@@ -53,7 +51,6 @@ def makePlot(filenames, country, tone):
         t=100,
         pad=5
     ))
-
     fig.update_layout(legend=dict(
         orientation="h",
         yanchor="bottom",
@@ -64,15 +61,12 @@ def makePlot(filenames, country, tone):
         margin=dict(
             l=0,
             r=50,
-        ))
-
-
-
+    ))
     #fig.write_image("fig1.png", width=1200, height=600, scale=1)
     fig.show()
 
 def main():
-    filenames = ['/Users/nathaniel/Desktop/Tone-Research/res/GuardianCSVs/BigCSV/Guardian-Business.csv','/Users/nathaniel/Desktop/Tone-Research/res/GuardianCSVs/BigCSV/Guardian-Opinion.csv']
+    filenames = ['res/GuardianCSVs/BigCSV/Business.csv','res/GuardianCSVs/BigCSV/Opinion.csv','res/GuardianCSVs/BigCSV/Science.csv','res/GuardianCSVs/BigCSV/Politics.csv']
     makePlot(filenames,"UK","Analytical")
 
 
