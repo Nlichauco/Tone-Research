b34777a40a410774544986bddbf03f2bd6804947
Nathaniel Lichauco <nathaniel@Nathaniels-MacBook-Pro.local>
2021-01-11 18:47:02 -0500

    Added plot for cumulative

--- AutoPlot.py
+++ AutoPlot.py
@@ -1,8 +1,8 @@
 import plotly.graph_objects as go
 import pandas as pd
 from plotly.validators.scatter.marker import SymbolValidator
-from IPython.display import Image
 
+import statistics as stat
 symbols = []
 raw_symbols = SymbolValidator().values
 for i in range(0, len(raw_symbols), 12):
@@ -184,6 +184,76 @@ def PercPlot(filenames,country,tone,tone_col,CovidData):
     fig.show()
 
 
+def TotalPlot(filenames,country,tone,tone_col,CovidData):
+    fig = go.Figure()
+    big=0
+    weeks = GetCols(filenames[0], 0)
+    # fig.update_layout(title=dict(bgcolor="#FFF" ))
+    for i in range(0, len(filenames)):
+        fname = filenames[i]
+        start = fname.rfind("/")
+        end = fname.rfind(".")
+        if fname.find("Guardian")!=-1:
+            fname=fname[start+1:end+1]
+            traceName = "GRD "+fname[:fname[i].find(".")]
+        else:
+            fname=fname[start+1:end+1]
+            traceName = "NYT "+fname[:fname[i].find(".")]
+        data = GetCols(filenames[i], tone_col)
+        fig.add_trace(
+            go.Scatter(x=weeks, y=data, name=traceName, yaxis="y", mode='lines+markers', marker_symbol=symbols[i]))
+        if big < max(data):
+            big=max(data)
+
+    fig.update_xaxes(title_text="Weeks", title_font_size=18, tickangle=90,range=[-.25, 42.15])
+    fig.update_yaxes(title_text=tone + " Tone Scores", title_font_size=18, range=[0, big+1])
+    fig.update_layout(title_text="<b>Cumulative " + tone + "Tone Score per Week</b>")
+    fig.update_layout(title_x=.01,paper_bgcolor="#FFF",
+    plot_bgcolor='rgba(0,0,0,0)')
+
+
+    if CovidData==True:
+        if country=="US":
+            Covid_Data = GetCols('res/CovidData/UScovidAVG.csv', 0)
+            high=250000
+        else:
+            Covid_Data = GetCols("res/CovidData/UKcovidAVG.csv", 0)
+            high=50000
+        fig.update_layout(yaxis2=dict(
+            title="Weekly Average Covid Cases in the " + country,
+            titlefont=dict(
+                size=17
+            ),
+            range=[0, high],
+            anchor="x",
+            overlaying="y",
+            side="right"
+        ))
+        fig.add_trace(go.Scatter(x=weeks, y=Covid_Data, name='Covid cases', yaxis='y2', fill='tozeroy'))
+
+    fig.update_layout(title_font_size=20,width=900,height=520, autosize=True,margin=dict(
+        l=100,
+        r=10,
+        b=100,
+        t=100,
+        pad=5
+    ))
+    fig.update_layout(legend=dict(
+        orientation="h",
+        yanchor="bottom",
+        y=1.02,
+        xanchor="right",
+        x=1
+    ),
+        margin=dict(
+            l=0,
+            r=50,
+        ))
+
+    # fig.write_image("fig1.png", width=1200, height=600, scale=1)
+    fig.show()
+
+
 
 
 
@@ -191,7 +261,8 @@ def main():
     filenames = ['res/NYT/Opinion.csv','res/GuardianCSVs/BigCSV/Opinion.csv']
     #makePlot(filenames, "UK", "Analytical")
     #crossPlot(filenames,"NYT","GRD","Analytical")
-    PercPlot(filenames,"UK","Sadness",10,CovidData=True)
+    TotalPlot(filenames,"UK","Sadness",9,CovidData=False)
+
 
 
 
