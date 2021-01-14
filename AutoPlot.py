956f501a6aad2c19d42502723f76b408ab9257b9
Folaido <feizr424@gmail.com>
2021-01-14 17:17:15 -0600

    add

--- AutoPlot.py
+++ AutoPlot.py
@@ -1,7 +1,10 @@
+import csv
+
 import plotly.graph_objects as go
 import pandas as pd
 from plotly.validators.scatter.marker import SymbolValidator
-from IPython.display import Image
+
+from BigCSV import sum_total
 
 symbols = []
 raw_symbols = SymbolValidator().values
@@ -33,11 +36,11 @@ def makePlot(filenames, country, tone):
     fig.update_yaxes(title_text=tone + " Tone Scores", title_font_size=18, range=[.5, 1], showgrid=False)
     # fig = make_subplots(specs=[[{"secondary_y": True}]])
 
-    Covid_Data = GetCols("/Users/nathaniel/Desktop/Tone-Research/res/CovidData/UKcovidAVG.csv", 0)
+    Covid_Data = GetCols("res/CovidData/UScovidAVG.csv", 0)
     fig.update_layout(yaxis2=dict(
         title="Weekly Average Covid Cases in the " + country,
         titlefont=dict(
-            color="#d62728"
+            size=18
         ),
         range=[0, 50000],
         anchor="x",
@@ -72,10 +75,99 @@ def makePlot(filenames, country, tone):
     fig.show()
 
 
+def get_row(filename):
+    t = []
+    with open(filename, newline='') as f:
+        reader = csv.reader(f)
+        next(reader)
+        body = next(reader)
+        for k in range(0, 7):
+            t.append(int(float(body[k])))
+
+    return t
+
+
+def overall_graph_cross_compare(desk):
+    source = ['New York Times', 'Guardian']
+    nyt = get_row("res/NYT/Overall/NYT_" + desk + ".csv")
+    guardian = get_row("res/Guardian/Overall/NYT_" + desk + ".csv")
+    fig = go.Figure(data=[
+        go.Bar(name='Anger', x=source, y=[nyt[0], guardian[0]]),
+        go.Bar(name='Sad', x=source, y=[nyt[1], guardian[1]]),
+        go.Bar(name='Fear', x=source, y=[nyt[2], guardian[2]]),
+        go.Bar(name='Joy', x=source, y=[nyt[3], guardian[3]]),
+        go.Bar(name='Analy', x=source, y=[nyt[4], guardian[4]]),
+        go.Bar(name='Confi', x=source, y=[nyt[5], guardian[5]]),
+        go.Bar(name='Tenta', x=source, y=[nyt[6], guardian[6]])
+    ])
+    graph_setup(fig)
+    fig.update_xaxes(tickfont=dict(size=18))
+    fig.update_layout(barmode='group',
+                      title_text=desk + " Cumulative Score Comparison " + ",New York Times VS Guardian",
+                      yaxis_title="Cumulative Tone Scores")
+    fig.show()
+
+
+def overall_graph_single_source(source):
+    desks = ['Business', 'Science', 'Politics', 'Opinion']
+    filenames = ['res/' + source + '/Overall/' + source + '_Business.csv',
+                 'res/' + source + '/Overall/' + source + '_Science.csv',
+                 'res/' + source + '/Overall/' + source + '_Politics.csv',
+                 'res/' + source + '/Overall/' + source + '_Opinion.csv']
+    t = [[], [], [], [], [], [], []]
+    for filename in filenames:
+        row = get_row(filename)
+        for k in range(0, 7):
+            t[k].append(row[k])
+
+    fig = go.Figure(data=[
+        go.Bar(name='Anger', x=desks, y=t[0]),
+        go.Bar(name='Sad', x=desks, y=t[1]),
+        go.Bar(name='Fear', x=desks, y=t[2]),
+        go.Bar(name='Joy', x=desks, y=t[3]),
+        go.Bar(name='Analy', x=desks, y=t[4]),
+        go.Bar(name='Confi', x=desks, y=t[5]),
+        go.Bar(name='Tenta', x=desks, y=t[6])
+    ])
+    graph_setup(fig)
+    fig.update_yaxes(range=[0, 1000])
+    fig.update_xaxes(tickfont=dict(size=18))
+    fig.update_layout(barmode='group',
+                      title_text="Overall Primary Desks Tone Scores For " + source,
+                      yaxis_title="Cumulative Tone Scores")
+    fig.show()
+
+
+def graph_setup(fig):
+    fig.update_layout(title_x=.1,
+                      yaxis_title_font_size=18,
+                      paper_bgcolor='rgba(0,0,0,0)',
+                      plot_bgcolor='rgba(0,0,0,0)', title_font_size=20, width=900, height=520, autosize=True,
+                      margin=dict(
+                          l=100,
+                          r=10,
+                          b=100,
+                          t=100,
+                          pad=5
+                      ),
+                      legend=dict(
+                          orientation="h",
+                          yanchor="bottom",
+                          y=1.02,
+                          xanchor="right",
+                          x=1
+                      ))
+
+
 def main():
-    filenames = ['/Users/nathaniel/Desktop/Tone-Research/res/GuardianCSVs/BigCSV/Guardian-Business.csv',
-                 '/Users/nathaniel/Desktop/Tone-Research/res/GuardianCSVs/BigCSV/Guardian-Opinion.csv']
-    makePlot(filenames, "UK", "Analytical")
+    filenames = ['res/NYT/Overall/NYT_Business.csv',
+                 'res/NYT/Overall/NYT_Science.csv',
+                 'res/NYT/Overall/NYT_Politics.csv',
+                 'res/NYT/Overall/NYT_Opinion.csv']
+    # makePlot(filenames, "US", "Analytical")
+    overall_graph_single_source(filenames, "New York Times")
+
+    # GetCols("res/NYT/Business.csv", 3)
 
 
 main()
--- BigCSV.py
+++ BigCSV.py
@@ -5,7 +5,7 @@ from natsort import natsorted
 from entity.Article import Article
 from entity.Week import Week
 
-the_paths = ['res/NYT/Science']
+the_paths = ['res/NYT/Business', 'res/NYT/Science', 'res/NYT/Opinion', 'res/NYT/Politics']
 
 
 def big_csv(paths):
@@ -76,25 +76,75 @@ def week_write(weeks, section):
     with open(file_name, 'w', newline='') as file:
         writer = csv.writer(file)
         writer.writerow(
-            ["Week", "Anger_max", "Anger_med", "Anger_avg", "Anger_total","Anger_ratio", "Sad_max", "Sad_median", "Sad_avg",
-             "Sad_total","Sad_ratio", "Fear_max", "Fear_med",
-             "Fear_avg", "Fear_total","Fear_ratio", "Joy_max", "Joy_med", "Joy_avg", "Joy_total","Joy_ratio", "Analy_max", "Analy_med",
-             "Analy_avg", "Analy_total","Analy_ratio", "Confi_max",
-             "Confi_med", "Confi_avg", "Confi_total","Confi_ratio", "Tenta_max", "Tenta_med", "Tenta_avg", "Tenta_total","Tenta_ratio"])
+            ["Week", "Anger_max", "Anger_med", "Anger_avg", "Anger_total", "Anger_ratio", "Sad_max", "Sad_median",
+             "Sad_avg",
+             "Sad_total", "Sad_ratio", "Fear_max", "Fear_med",
+             "Fear_avg", "Fear_total", "Fear_ratio", "Joy_max", "Joy_med", "Joy_avg", "Joy_total", "Joy_ratio",
+             "Analy_max", "Analy_med",
+             "Analy_avg", "Analy_total", "Analy_ratio", "Confi_max",
+             "Confi_med", "Confi_avg", "Confi_total", "Confi_ratio", "Tenta_max", "Tenta_med", "Tenta_avg",
+             "Tenta_total", "Tenta_ratio"])
         for week in weeks:
             writer.writerow(
                 [week.weekname, week.Anger.get_max(), week.Anger.get_median(), week.Anger.get_mean(),
-                 week.Anger.get_total(),week.Anger.get_perc(),
-                 week.Sadness.get_max(), week.Sadness.get_median(), week.Sadness.get_mean(), week.Sadness.get_total(),week.Sadness.get_perc(),
+                 week.Anger.get_total(), week.Anger.get_perc(),
+                 week.Sadness.get_max(), week.Sadness.get_median(), week.Sadness.get_mean(), week.Sadness.get_total(),
+                 week.Sadness.get_perc(),
                  week.Fear.get_max(),
-                 week.Fear.get_median(), week.Fear.get_mean(), week.Fear.get_total(),week.Fear.get_perc(), week.Joy.get_max(),
+                 week.Fear.get_median(), week.Fear.get_mean(), week.Fear.get_total(), week.Fear.get_perc(),
+                 week.Joy.get_max(),
                  week.Joy.get_median(),
-                 week.Joy.get_mean(), week.Joy.get_total(),week.Joy.get_perc(), week.Analy.get_max(), week.Analy.get_median(),
-                 week.Analy.get_mean(), week.Analy.get_total(),week.Analy.get_perc(),
+                 week.Joy.get_mean(), week.Joy.get_total(), week.Joy.get_perc(), week.Analy.get_max(),
+                 week.Analy.get_median(),
+                 week.Analy.get_mean(), week.Analy.get_total(), week.Analy.get_perc(),
                  week.Confi.get_max(),
-                 week.Confi.get_median(), week.Confi.get_mean(), week.Confi.get_total(), week.Confi.get_perc(), week.Tenta.get_max(),
+                 week.Confi.get_median(), week.Confi.get_mean(), week.Confi.get_total(), week.Confi.get_perc(),
+                 week.Tenta.get_max(),
                  week.Tenta.get_median(),
-                 week.Tenta.get_mean(), week.Tenta.get_total(),week.Tenta.get_perc()])
+                 week.Tenta.get_mean(), week.Tenta.get_total(), week.Tenta.get_perc()])
 
 
-big_csv(the_paths)
+def sum_total(path):
+    missing_values = ["NaN", "0"]
+    # print(os.listdir(path))
+    sums = list()
+    for i in range(0, 7):
+        sums.append(0)
+    for filename in natsorted(os.listdir(path), key=lambda y: y.lower()):
+        if not filename.startswith('.'):
+            with open(os.path.join(path, filename)) as f:
+
+                data = pd.read_csv(f, header=0, sep=r'\s*,\s*', na_values=missing_values, engine='python',
+                                   usecols=[5, 6, 7, 8, 9, 10, 11])
+
+                for ind in data.index:
+                    if data['Analy'][ind] > 0.5:
+                        sums[0] += data['Analy'][ind]
+                    if data['Sad'][ind] > 0.5:
+                        sums[1] += data['Sad'][ind]
+                    if data['Confi'][ind] > 0.5:
+                        sums[2] += data['Confi'][ind]
+                    if data['Anger'][ind] > 0.5:
+                        sums[3] += data['Anger'][ind]
+                    if data['Tenta'][ind] > 0.5:
+                        sums[4] += data['Tenta'][ind]
+                    if data['Fear'][ind] > 0.5:
+                        sums[5] += data['Fear'][ind]
+                    if data['Joy'][ind] > 0.5:
+                        sums[6] += data['Joy'][ind]
+    return sums
+
+
+def write_overall(path, source, desk):
+    sums = sum_total(path)
+    file_name = source + "_" + desk + ".csv"
+    with open(file_name, 'w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(
+            ["Anger", "Sad", "Fear", "Joy", "Analy", "Confi", "Tenta"])
+        writer.writerow([sums[0], sums[1], sums[2], sums[3], sums[4], sums[5], sums[6]])
+
+
+for p in the_paths:
+    write_overall(p, "NYT", p[p.rfind("/") + 1:])
+# big_csv(the_paths)
